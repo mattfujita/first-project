d90027456318878a53763e01cc2ccdd1d2d28210
Matt Fujita <matthew.fujita@libertymutual.com>
2017-08-11 10:20:07 -0700

    code refactoring

--- src/main/java/com/example/demo/controllers/MathController.java
+++ src/main/java/com/example/demo/controllers/MathController.java
@@ -6,27 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.example.demo.models.Adder;
-import com.example.demo.models.Yeller;
 
 @Controller
 @RequestMapping("/math") //this is the base path for the controller, we NEED TO SPECIFY FOR ROUTING, we are appending this now in URL
 public class MathController {
 
-	//this points to http://localhost:8080/adder (this is a post mapping)
+	//this points to http://localhost:8080/math/adder (this is a post mapping)
 	@PostMapping("adder")
 	public String addTwoNumbers(@RequestParam(name = "left") int first, @RequestParam(name = "right") double second,
 			Model model) {
 		Adder adder = new Adder(first, second);
 		double result = adder.calculate();
 		model.addAttribute("sum", result);
-		return "helloworld/sum-result";
+		return "mathematics/sum-result";
 	}
 	
-	@GetMapping("adder")
+	@GetMapping("adder") //user is typing in the URL directly
 	public String adders() {
-		return "helloworld/adder-form";
+		return "mathematics/adder-form";
 	}
 }
