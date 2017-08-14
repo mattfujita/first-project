f3964f465a1d7e8d4a11d58d0c25d3aa507ca435
Matt Fujita <matthew.fujita@libertymutual.com>
2017-08-14 09:39:10 -0700

    code refactoring

--- src/main/java/com/example/demo/models/Calculation.java
+++ src/main/java/com/example/demo/models/Calculation.java
@@ -2,44 +2,42 @@ package com.example.demo.models;
 
 public class Calculation {
 	//fields or instance variables
-	private double addition;
-	private double subtraction;
-	private double multiplication;
-	private double divide;
-	private double modulus;
-	private double exponent;
+	private double first;
+	private double second;
 	
 	//the constructor 
 	public Calculation(int first, double second) {
-			addition = first + second;
-			subtraction = first - second;
-			multiplication = first * second;
-			divide = first / second;
-			modulus = first % second;
-			exponent = Math.pow(first, second);
+			this.first = first;
+			this.second = second;
 	}
 	
 	public double addition() {
+		double addition = first + second;
 		return addition;
 	}
 	
 	public double subtraction() {
+		double subtraction = first - second;
 		return subtraction;
 	}
 	
 	public double multiplication() {
+		double multiplication = first * second;
 		return multiplication;
 	}
 	
 	public double divide() {
+		double divide = first / second;
 		return divide;
 	}
 	
 	public double modulus() {
+		double modulus = first % second;
 		return modulus;
 	}
 	
 	public double exponent() {
+		double exponent = Math.pow(first, second);
 		return exponent;
 	}
 }
